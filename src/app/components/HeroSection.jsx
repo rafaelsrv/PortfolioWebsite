22113b527fcb7e8c8232ae58a5a5c27f37699e81
RafaelSRV <rafael-srv@hotmail.com>
2024-02-04 22:28:35 -0300

    Atualizando Portfólio

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -61,9 +61,9 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/profilephoto.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="absolute rounded-full transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 "
               width={300}
               height={300}
             />
