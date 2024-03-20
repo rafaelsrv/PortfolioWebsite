6782ca08b3cec2332a1f22b4a10b026982a7b28a
RafaelSRV <rafael-srv@hotmail.com>
2024-03-19 23:59:10 -0300

    Subindo Link de currículo

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -24,7 +24,7 @@ const HeroSection = () => {
               sequence={[
                 "Rafael",
                 1000,
-                "Dev Front-End",
+                "Dev FullStack",
                 1000
               ]}
               wrapper="span"
@@ -33,8 +33,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+            
           </p>
           <div>
             <Link
@@ -43,14 +42,14 @@ const HeroSection = () => {
             >
               Contato
             </Link>
-            <Link
-              href="/"
+            <a
+              href="/images/projects/CurriculoRafaelSaraiva.pdf" download={true}
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
-            </Link>
+            </a>
           </div>
         </motion.div>
         <motion.div
